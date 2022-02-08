b90c40e4001f49dfdf636b7487ee57f457847593
=rnin9 <=rkdalswn1209>
2022-02-08 12:52:37 +0900

    basic Scalar type

--- typeDefs-resolvers/equipments.js
+++ typeDefs-resolvers/equipments.js
@@ -8,10 +8,28 @@ const typeDefs = gql`
         count: Int
         new_or_used: String
     }
+    type EquipmentAdv{
+        id: ID!
+        used_by: String!
+        count: Int!
+        use_rate: Float
+        is_new: Boolean!
+    }
 `
+//ID === String이지만, 데이터의 고유 식별자.
+//! === non - null 
 const resolvers = {
     Query: {
         equipments: (parent, args) => dbWorks.getEquipments(args),
+        equipmentAdvs: (parents, args) =>
+            dbWorks.getEquipments(args)
+                .map((equipment) => {
+                    if (equipment.used_by === 'developer') {
+                        equipment.use_rate = Math.random().toFixed(2)
+                    }
+                    equipment.is_new = equipment.new_or_used === 'new'
+                    return equipment
+                })
     },
     Mutation: {
         deleteEquipment: (parent, args) => dbWorks.deleteItem('equipments', args),
