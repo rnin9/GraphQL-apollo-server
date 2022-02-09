b4c6794e2d46cfffe2285608e4e17ad9c3a2ee99
=rnin9 <=rkdalswn1209>
2022-02-09 13:01:41 +0900

    feature/ApolloClient Setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import React, { useState } from 'react';
+import { ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 import Roles from './components/roles'
 import Teams from './components/teams'
 import People from './components/people'
+
+const client = new ApolloClient({
+  uri: `http://localhost:4000`,
+  cache: new InMemoryCache()
+  //한번 받은 데이터를 필요이상으로 관리하지 않아도 되게끔 cache 사용
+})
 
 function App() {
 
@@ -28,17 +36,19 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Company Management</h1>
-        <nav>
-          <ul>
-            {NavMenus()}
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {mainComp[menu]}
-      </main>
+      <ApolloProvider client={client}>
+        <header className="App-header">
+          <h1>Company Management</h1>
+          <nav>
+            <ul>
+              {NavMenus()}
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {mainComp[menu]}
+        </main>
+      </ApolloProvider >
     </div>
   );
 }
